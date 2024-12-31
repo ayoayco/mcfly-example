1f47806254737aee540c620017c66c332527277b
Ayo Ayco <ayo@ayco.io>
2024-12-31 11:33:41 +0100

    initial commit

---
+++ routes/[...].js
@@ -0,0 +1,10 @@
+/**
+ * McFly SSR logic
+ * 👋 this is not the route you're looking for
+ * ...pages are in ./src/pages
+ * ...reusable code are in ./src/components
+ * @see https://ayco.io/gh/McFly#special-directories
+ */
+
+import { useMcFlyRoute } from '@mcflyjs/core'
+export default useMcFlyRoute({ storage: useStorage() })
